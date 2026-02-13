2e25e33558ec1643484ccfd12c7c3e01a50189b5
illa-j <iliacdmn@gmail.com>
2026-02-13 17:53:54 +0200

    feat: remove unused import of get_user_model from tasks.py

--- core/tasks.py
+++ core/tasks.py
@@ -2,7 +2,6 @@ from datetime import timedelta
 
 from celery import shared_task
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.utils import timezone
 
